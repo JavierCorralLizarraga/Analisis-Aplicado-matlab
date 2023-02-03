72b78a3d050b554662d1eb9b4c099b34eb7dfdcf
JavierCorralLizarraga <javiercorral@hotmail.com>
2023-02-02 19:46:47 -0600

    archivos prof

--- rosenbrock.m
+++ rosenbrock.m
@@ -1,9 +1,9 @@
 function [fx] = rosenbrock(x)
-% Funcion de Rosenbrock para probar algoritmos de minimizacion
-% Analisis Aplicado
+% Función de Rosenbrock para probar algoritmos de minimización
+%   Análisis Aplicado
 % ITAM
 % 19 de enero de 2023
 
 a = x(1); b = x(2);
-fx = 100 * ( b - a^2 )^2 + (1-a)^2;
+fx = 100*(b-a^2)^2 + ( 1- a)^2;
 end
